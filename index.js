eefb13d472f351a7d41fd39ee025da34e023fc1b
rikiarizona <rikiarizona@gmail.com>
2022-08-03 09:35:30 +0700

    TUGAS 7 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,9 @@
+var tinggiBadan = [140,145,150,155,160,165,170,175,180,185];
+for(var i=0;i<tinggiBadan.length;i++){
+    console.log(tinggiBadan[i])
+}
+console.log()
+
+for(let x of tinggiBadan){
+    console.log(x)
+}
